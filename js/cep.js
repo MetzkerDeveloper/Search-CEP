b14d5a98961687e30a46c5f89daf88b333080acd
Waleriano Metzker <walerianoadm@gmail.com>
2023-03-29 21:18:59 -0300

    Atualização código

--- js/cep.js
+++ js/cep.js
@@ -7,31 +7,38 @@ const inputAddress = document.querySelector("#address");
 // funções
 
 const getCep = async (cep)=>{
+    if(inputCep.value.length < 9 ){
+
+    }
+    else{
     const apiCep = ` https://cdn.apicep.com/file/apicep/${cep}.json`;
     const response = await fetch(apiCep);
     const data = await response.json();
     return data ;
-}
+   
+}};
 
 const showCEP = async (cep)=>{
-      
     const data = await getCep(cep);
     
-    inputState.value = data.state
-    inputCity.value = data.city
-    inputDistrict.value = data.district
-    inputAddress.value = data.address
+        if(inputCep.value.length < 9){
+            alert("Preencha um CEP válido!")
+        }
+        else{
+        inputState.value = data.state
+        inputCity.value = data.city
+        inputDistrict.value = data.district
+        inputAddress.value = data.address
 }
-
+} 
 // evento
 
-// inputCep.addEventListener("blur", (e)=>{
-//      e.preventDefault()
-
-//  const cep = inputCep.value;
-   
-//  showCEP(cep)
-// })
+ inputCep.addEventListener("blur", (e)=>{
+      e.preventDefault()
+  const cep = inputCep.value;
+ 
+ showCEP(cep)
+ })
 
 inputCep.addEventListener("keyup", (e)=>{
     if(e.code ==="Enter"){
@@ -40,4 +47,11 @@ inputCep.addEventListener("keyup", (e)=>{
         showCEP(cep);
 
     }
+})
+
+inputCep.addEventListener('keypress', ()=>{
+    let input = inputCep.value.length;   
+    if(input === 5 ){
+        inputCep.value += "-"
+     }
 })
